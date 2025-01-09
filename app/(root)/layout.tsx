a4ed9f6213663f224384dfe250d7ef07755801c8
Akash <akash2904r@gmail.com>
2025-01-09 22:15:45 +0530

    Add root group

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,31 @@
+import { redirect } from "next/navigation";
+
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import Sidebar from "@/components/Sidebar";
+import { getCurrentUser } from "@/lib/actions/user.action";
+
+export default async function RootLayout({ 
+    children 
+}: { 
+    children: React.ReactNode 
+}) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) return redirect("/sign-in");
+
+    return (
+        <main className="flex h-screen">
+            <Sidebar {...currentUser} />
+
+            <section className="flex h-full flex-1 flex-col">
+                <MobileNavigation />
+                <Header />
+
+                <div className="main-content">
+                    {children}
+                </div>
+            </section>
+        </main>
+    );
+}
