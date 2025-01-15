81a0e1dd48ead4233409dd784f37e33d2a4d1d2c
Akash <akash2904r@gmail.com>
2025-01-15 21:37:31 +0530

    Set up basic functionality for file uploading

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,7 +1,57 @@
-export default function FileUploader() {
+"use client";
+
+import { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+
+interface Props {
+    ownerId: string;
+    accountId: string;
+    className: string;
+}
+
+export default function FileUploader({ 
+    ownerId,
+    accountId,
+    className 
+}: Props) {
+    const [files, setFiles] = useState<File[]>([]);
+
+    const onDrop = useCallback((acceptedFiles) => {
+        // Do something with the files
+    }, [])
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    
     return (
-        <div>
-            File Uploader
+        <div {...getRootProps()} className="cursor-pointer">
+            <input {...getInputProps()} />
+            <Button type="button" className={cn('uploader-button', className)}>
+                <Image 
+                    src="/icons/upload.svg"
+                    alt="upload"
+                    width={24}
+                    height={24}
+                />
+                <p>Upload</p>
+            </Button>
+            {files.length > 0 && (
+                <ul className="uploader-preview-list">
+                    <h4 className="h4 text-light-100">Uploading</h4>
+
+                    {files.map((file, index) => {
+                        const { type, extension } = getFileType(file.name);
+                    })}
+                </ul>
+            )}
+
+            {
+                isDragActive ?
+                <p>Drop the files here ...</p> :
+                <p>Drag 'n' drop some files here, or click to select files</p>
+            }
         </div>
-    );
+    )
 }
