f0eab3b4db9a38a6578a0c819f569cbd05b53884
Akash <akash2904r@gmail.com>
2025-03-05 20:26:59 +0530

    Fetch searchParams and pass them to getFiles

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,14 +1,18 @@
 import { Models } from "node-appwrite";
 
 import { getFiles } from "@/lib/actions/file.action";
+import { getFileTypesParams } from "@/lib/utils";
 
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 
-export default async function Page({ params }: SearchParamProps) {
-    const type = (await params)?.type as string;
+export default async function Page({ searchParams, params }: SearchParamProps) {
+    const type = ((await params)?.type as string) || "";
+    const searchText = ((await searchParams)?.query as string) || "";
+    const sort = ((await searchParams)?.sort as string) || "";
 
-    const files = await getFiles();
+    const types = getFileTypesParams(type) as FileType[];
+    const files = await getFiles({ types, searchText, sort });
 
     return (
         <div className="page-container">
